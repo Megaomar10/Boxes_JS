c04a5bfe4ba0f75e5dc982a3dccf314a2fb201fe
Iberocontact <contacto@iberobienes.com>
2021-10-05 14:54:06 -0500

    Add a new button

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const cajas = document.querySelectorAll('.cajas');
 const btn_Par = document.querySelector('#btn_Par');
 const btn_Impar = document.querySelector('#btn_Impar');
 const btn_Clear = document.querySelector('#btn_Clear');
+const btn_Contar = document.querySelector('#btn_Contar');
 
-//Botón pares
+//Botón Pares
 btn_Par.addEventListener('click', () => {
     for (let i = 0; i <= cajas.length; i++) {
         if (i % 2 == 0) {
@@ -13,7 +14,7 @@ btn_Par.addEventListener('click', () => {
     }
 });
 
-//Boton impares
+//Boton Impares
 btn_Impar.addEventListener('click', () => {
     for (let i = 0; i <= cajas.length; i++) {
         if (i % 2 != 0) {
@@ -22,12 +23,20 @@ btn_Impar.addEventListener('click', () => {
     }
 });
 
+//Boton Contar
+btn_Contar.addEventListener('click', () => {
+    for (let i = 0; i <= cajas.length; i++) {
+
+        cajas[i].innerHTML = i;
+
+    }
+});
 
 //Boton limpiar
 btn_Clear.addEventListener('click', () => {
     for (let i = 0; i <= cajas.length; i++) {
 
         cajas[i].style.background = "silver";
-
+        cajas[i].innerHTML = "";
     }
 });
